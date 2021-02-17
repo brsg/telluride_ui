545efcc3e289beaa3cc59039c9acbf452e813aca
Dave Muirhead <dave@brsg.io>
2021-02-17 15:44:28 -0500

    renamed PipelineConfig to Pipeline

---
+++ lib/telluride_ui/pipeline/pipeline.ex
@@ -0,0 +1,68 @@
+defmodule Telluride.Pipeline.Pipeline do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias __MODULE__
+
+  embedded_schema do
+    field :producer_concurrency, :integer
+    field :processor_concurrency, :integer
+    field :batcher1_concurrency, :integer
+    field :batcher2_concurrency, :integer
+    field :batcher1_batch_size, :integer
+    field :batcher2_batch_size, :integer
+    field :rate_limit_allowed, :integer
+    field :rate_limit_interval, :integer
+    field :node_status, :map
+  end
+
+  def new do
+    %Pipeline{
+      producer_concurrency: 1,
+      processor_concurrency: 1,
+      batcher1_concurrency: 1,
+      batcher2_concurrency: 1,
+      batcher1_batch_size: 1,
+      batcher2_batch_size: 1,
+      rate_limit_allowed: 50,
+      rate_limit_interval: 1_000,
+      node_status: %{}
+    }
+  end
+
+  @doc false
+  def changeset(%Pipeline{} = pipeline, attrs \\ %{}) do
+
+    pipeline
+    |> cast(attrs, [
+      :producer_concurrency,
+      :processor_concurrency,
+      :batcher1_concurrency,
+      :batcher2_concurrency,
+      :batcher1_batch_size,
+      :batcher2_batch_size,
+      :rate_limit_allowed,
+      :rate_limit_interval,
+      :node_status
+    ])
+    |> validate_required([
+      :producer_concurrency,
+      :processor_concurrency,
+      :batcher1_concurrency,
+      :batcher2_concurrency,
+      :batcher1_batch_size,
+      :batcher2_batch_size,
+      :rate_limit_allowed,
+      :rate_limit_interval,
+    ])
+    |> validate_number(:producer_concurrency, greater_than: 0)
+    |> validate_number(:processor_concurrency, greater_than: 0)
+    |> validate_number(:batcher1_concurrency, greater_than: 0)
+    |> validate_number(:batcher2_concurrency, greater_than: 0)
+    |> validate_number(:batcher1_batch_size, greater_than: 0)
+    |> validate_number(:batcher2_batch_size, greater_than: 0)
+    |> validate_number(:rate_limit_allowed, greater_than: 0)
+    |> validate_number(:rate_limit_interval, greater_than: 0)
+  end
+
+end
